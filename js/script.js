7f486e2daaadc0795e34af7d74514e8f4ec3887f
Adam Camp <adam.camp@utah.edu>
2023-08-29 16:00:30 -0600

    update index.html, script.js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,9 @@
-/**
- * Checks if the user's preferred color scheme is dark and sets the HTML attribute accordingly.
- */
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+var localStorage = window.localStorage;
+var darkMode = localStorage.getItem("darkMode");
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
   document.getElementsByTagName("html")[0].attributes["data-bs-theme"].value =
     "dark";
 }
-
-/**
- * Retrieves the user's settings from local storage and sets the HTML attribute accordingly
- */
-var localSettings = localStorage.getItem("settings");
-if (localSettings) {
-  var settings = JSON.parse(localSettings);
-  if (settings.theme) {
-    document.getElementsByTagName("html")[0].attributes["data-bs-theme"].value =
-      settings.theme;
-  }
-}
-
-/**
- * Saves the user's current theme setting to local storage before the page is unloaded.
- */
-window.addEventListener("beforeunload", function () {
-  var settings = {
-    theme:
-      document.getElementsByTagName("html")[0].attributes["data-bs-theme"]
-        .value,
-  };
-  localStorage.setItem("settings", JSON.stringify(settings));
-});
-
